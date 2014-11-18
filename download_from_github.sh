2bb57cb57fc1d8e3517109f7cbe86b599fbcb77e
Zac Pierson <zacpiersonhehe@gmail.com>
2014-11-18 11:43:51 -0600

    Install script pls

--- download_from_github.sh
+++ download_from_github.sh
@@ -3,7 +3,7 @@ hasgit="yes"
 cd ~/
 mkdir .icons >/dev/null 2>&1
 cd .icons
-command -v git >/dev/null 2>&1 || { hasgit="no" }
+command -v git >/dev/null 2>&1 || { hasgit="no"; }
 if [ "$hasgit" == "no" ]; then
 echo Can\'t find git.  Downloading from zip file.
 wget https://github.com/zacpier/GnomeYosemiteIcons/archive/master.zip
