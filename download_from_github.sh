d84772ce18e754db1f9b74f8268ea42b6131395c
Zac Pierson <zacpiersonhehe@gmail.com>
2014-11-18 11:42:40 -0600

    Install script round two

--- download_from_github.sh
+++ download_from_github.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
+hasgit="yes"
 cd ~/
 mkdir .icons >/dev/null 2>&1
 cd .icons
-command -v git >/dev/null 2>&1 || {
+command -v git >/dev/null 2>&1 || { hasgit="no" }
+if [ "$hasgit" == "no" ]; then
 echo Can\'t find git.  Downloading from zip file.
 wget https://github.com/zacpier/GnomeYosemiteIcons/archive/master.zip
 unzip -o master.zip
-} else {
+else
 echo Cloning from GitHub repository.
 git clone https://github.com/zacpier/GnomeYosemiteIcons.git GnomeYosemiteIcons-master
-}
+fi
 cd GnomeYosemiteIcons-master
 rm download_from_github.sh
 gsettings set org.gnome.desktop.interface icon-theme 'GnomeYosemiteIcons'
